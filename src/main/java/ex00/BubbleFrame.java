84179a4cb3def6e44406251e97c2212f4b6c6172
vosw1 <vosw1@naver.com>
2024-02-02 23:26:35 +0900

    창 띄우기

---
+++ src/main/java/ex00/BubbleFrame.java
@@ -0,0 +1,14 @@
+package ex00;
+
+import javax.swing.*;
+
+public class BubbleFrame extends JFrame {
+    public BubbleFrame() {
+        setSize(1000, 640);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new BubbleFrame();
+    }
+}
